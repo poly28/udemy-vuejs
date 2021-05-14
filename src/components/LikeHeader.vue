ffee11ab51c98b686a1a9caf5066479a03994354
poly28 <horimasa0328@gmail.com>
2021-05-14 17:09:47 +0900

    #103まで

---
+++ src/components/LikeHeader.vue
@@ -0,0 +1,18 @@
+<template>
+    <!-- <h1>{{headerText}}</h1> -->
+    <div>
+        <slot>
+            <h2>
+            デフォルトのタイトル
+            </h2>
+        </slot> 
+    </div>
+</template>
+
+
+<script>
+export default {
+    props:["headerText"],
+}
+
+</script>
